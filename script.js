88105440019c67f168523ada8be1f8ff63ebaddc
DimaTykva <dimatykvadanait@gmail.com>
2023-03-12 13:49:21 +0200

    dz_28 v2

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let mainBox = document.querySelector('.main_info p');
-mainBox.onmouseover = function () {
-    console.log('hello')
-    mainBox.insertAdjacentHTML('afterend','<div class="popup"><p>Hello man!!</p></div>')
-}
+let messageBlock = document.querySelector('.message_block');
+// mainBox.onmouseover = function () {
+//     console.log('hello')
+//     mainBox.insertAdjacentHTML('afterend','<div class="popup"><p>Hello man!!</p></div>')
+// }
+//
+// mainBox.onmouseout = function () {
+//     console.log('goodbay')
+//     let parentEl = (document.querySelector('.popup')).closest('.main_info')
+//     parentEl.removeChild(document.querySelector('.popup'))
+// }
 
-mainBox.onmouseout = function () {
-    console.log('goodbay')
-    let parentEl = (document.querySelector('.popup')).closest('.main_info')
-    parentEl.removeChild(document.querySelector('.popup'))
-}
+mainBox.addEventListener('mouseover', function (){
+    messageBlock.classList.add('show')
+})
+mainBox.addEventListener('mouseout', function (){
+    messageBlock.classList.remove('show')
+})
